69a85810bbbbce202d544fb9576fe90666717cf2
olhadyriv <125921667+olhadyriv@users.noreply.github.com>
2024-04-07 16:55:34 +0300

    driver_and_login_functions_are_united

--- login.py
+++ login.py
@@ -10,17 +10,13 @@ email = input("Enter your email: ")
 password = input("Enter your password: ")
 days = input("Enter a number of days of subscription: ")
 
-#Викликаємо драйвер Google Chrome на максимальному розширенні екрана з затримкою в 1 секунду
-def ex_browser():
-
+#Викликаємо драйвер Google Chrome на максимальному розширенні екрана з затримкою в 1 секунду в одній функції з логіном
+#Прописується введений юзером аргумент зі змінної "email" у поле "Enter email"
+def login(email, password):
     options = Options()
     options.add_argument('--start-maximized')
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
-    return driver
-
-#Прописується введений юзером аргумент зі змінної "email" у поле "Enter email"
-def login(email, password, driver):
     driver.get("https://freevpnplanet.com/login/")
     time.sleep(1)
     driver.find_element(By.XPATH, '//*[@id="__layout"]/div/main/div[2]/div[1]/div[1]/div[2]/div/label/input').send_keys(email)
@@ -29,6 +25,7 @@ def login(email, password, driver):
 #Клік на кнопку "Login" і затримка в 3 секунди
     driver.find_element(By.XPATH,'//*[@id="__layout"]/div/main/div[2]/div[1]/div[1]/button').click()
     time.sleep(3)
+    return driver
 def cabinet(driver, days):
 #Забираємо текстове значення з поля "Number of days" і затримка 2 секунди перед закриттям браузера
     subscription = driver.find_element(By.XPATH, value='//*[@id="__layout"]/div/div[2]/div/div[2]/div[1]/div[2]/div/div[1]/'
@@ -41,8 +38,7 @@ def cabinet(driver, days):
 # Виводимо текст при умові різних значень аргументу змінної "days" і даних змінної "Subscription"
     assert days == subscription, "The correct number of your subscription days is: " + str(subscription)
 
-driver = ex_browser()
-login(email, password, driver)
+driver = login(email, password)
 cabinet(driver, days)
 driver.close()
 driver.quit()
